3ab90062e615a2169d91387dec8ec44ce4ff7680
Hammad <m.hammadsaani@gmail.com>
2024-07-15 23:14:12 +0500

    Week 3 files added

---
+++ week 3/2.py
@@ -0,0 +1,16 @@
+# print("My name is",end="")
+# print("Moosa")
+
+
+def name(name1, name2):
+    print(name1+name2, end=" ")
+    
+    
+def age(age1):
+     print(age1)
+     
+     
+print('my name is', end=' ') 
+name('moosa'," kamal") 
+print( "and my age is", end=' ') 
+age(13)
