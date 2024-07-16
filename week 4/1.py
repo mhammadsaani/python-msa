f57186708d50269d7abac62743fa8b4a82078624
Hammad <m.hammadsaani@gmail.com>
2024-07-16 23:57:37 +0500

    week 4 class 1, tuesday

---
+++ week 4/1.py
@@ -0,0 +1,10 @@
+"""
+instead of printing the result, we want to return it. 
+Then, store the returned thing in a variable and multiply it with 5 and 
+then print it
+"""
+
+def sum(num1, num2):
+    result = num1 + num2
+    
+print(sum(2, 3))
