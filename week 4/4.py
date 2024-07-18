2c03ff9177e7869a6ec20b92bd2498293b3ee969
Hammad <m.hammadsaani@gmail.com>
2024-07-19 00:17:06 +0500

    week 4 class 3

---
+++ week 4/4.py
@@ -0,0 +1,13 @@
+# loops => while, for  
+
+"""
+while -> I want to repeat this line untill Moosa Sleeps
+for   -> I want to repeat this line 10 times
+"""
+
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in range(10):
+    print(i)
+
+
+    
