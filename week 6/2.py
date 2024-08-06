37fe5311b626d3a9a51b1e92610dccb2f4ce5534
Hammad <m.hammadsaani@gmail.com>
2024-08-07 00:02:51 +0500

    Week 6, lecture 2

---
+++ week 6/2.py
@@ -0,0 +1,44 @@
+# # line of code that represents an action --> statement
+
+# # combination of values, operators and variables  --> expression
+
+# print("Hello World") # statement -> print ing hello world is an action
+
+# a = 10  # expression -> 
+
+# num = 10 # expression -> 
+
+
+# if False: # statement -> action of checking a condition
+#     print("Hello")  # statement
+    
+    
+# # expression is something that produces a value,
+# # statement is a whole line of code that doesn't print result
+
+
+
+# print(2 + 3)
+
+
+# recursion -> a function calling itself
+
+
+# def myFn():
+#     print('Hello')
+#     myFn()
+      
+# myFn()
+
+
+
+
+
+name = input("Enter your name: ")
+
+name = int(name)
+
+print(type(name))
+
+
+
