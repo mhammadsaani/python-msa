137df1869e2b677fe22e55d438e0073b4dde11a8
Hammad <m.hammadsaani@gmail.com>
2024-08-08 12:51:28 +0500

    Week 6, lecture 3

---
+++ week 6/3.py
@@ -0,0 +1,35 @@
+
+"""
+RETURN KEYWORD
+    return can be used inside a function only
+    if a function doesn't have return keyword, then it is returning None
+    if there are some instructions after return keyword, they will not be executed.
+    if you want to check that either a function is returning something or not, you call the function inside a print statement.
+    We can assign the returned value to a variable.
+"""
+
+
+# def add(num1, num2):
+#     return num1 + num2
+
+
+# result = add(10, 20)
+
+# print(result)
+
+
+# def absolute_value(x):
+#     if x < 0:
+#         return -x
+#     else:
+#         return x 
+
+# print(absolute_value(6))
+
+
+
+for i in range(5, 10, 2):
+    print(i)
+    
+    
+    
