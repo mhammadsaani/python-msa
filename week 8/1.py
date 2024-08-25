ec13ed41825ccc12f008b688f61477bca7bfb849
Hammad <m.hammadsaani@gmail.com>
2024-08-26 00:02:21 +0500

    W#8_1

---
+++ week 8/1.py
@@ -0,0 +1,33 @@
+### String ###
+
+# Anything inside double, single and triple quotes is a string
+
+## why do we use strings?
+
+# method -> a method is like a function  but it has some additional things as well.
+
+text = "this is text"
+
+name1 = "Hammad"
+name2 = "Abdullah"
+
+print(text.index("s"))
+
+
+"""
+
+Method	Description
+capitalize()	Converts the first character to upper case
+count()	Returns the number of times a specified value occurs in a string
+endswith()	Returns true if the string ends with the specified value
+find()	Searches the string for a specified value and returns the position of where it was found
+format()	Formats specified values in a string
+index()	Searches the string for a specified value and returns the position of where it was found
+split()	Splits the string at the specified separator, and returns a list
+startswith()	Returns true if the string starts with the specified value
+"""
+
+
+number = 10
+
+print(number)
