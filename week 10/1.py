d2cb7a151db8be91ad727e281628d3152427dd50
Hammad <m.hammadsaani@gmail.com>
2024-09-08 23:50:45 +0500

    Week 10, Both lectures

---
+++ week 10/1.py
@@ -0,0 +1,69 @@
+# Data Structure 
+
+# Data structures are used for storing data and organizing!
+
+# variables -> stores data like 20 years old. 
+
+# print age, then it will print 25
+
+
+## strings ==>  
+
+# Computer => Cabinet -> Salt, Pepper
+# text -> Computer ? 
+# print("Hammad".count("a"))
+
+
+
+
+marks = [61, 23, 61]
+
+
+# append_item = marks.append(20) # put item to last of the list
+# clears_items = marks.clear()  # delete all the items from the list
+# copy_list = marks.copy()      # copy all the items into new list and then we can store it in variable
+# count_item_occurence = marks.count(61)  # count the occurence of item given as an argument
+# print(count_item_occurence)
+
+# marks.extend([45, 50, 55])
+# print(marks)
+
+
+
+
+
+# def hammads_extends(myList, iterable):
+    
+#     for x in iterable:
+#         myList.append(x)
+        
+        
+#     return myList
+
+
+
+# print(hammads_extends(marks, 'james'))
+
+
+# 'j'
+# 'a'
+# 'm'
+# 'e'
+# 's'
+
+
+
+
+# marks.index()
+
+
+
+
+
+# a = [1, 2, 3]
+
+# b = a.copy()
+
+# b.append(30)
+
+# print(a, b)
---
+++ week 10/2.py
@@ -0,0 +1,27 @@
+# Task
+
+# fruits = [1, 2, 3, 1]
+
+# fruits.sort(reverse=True)
+
+# print(fruits)
+
+
+
+
+# for i in range(10):
+#     print("hey")
+    
+    
+# for i in [1, 2, 3]:
+#     print(i)
+
+
+
+
+
+marks = [40, 21, 43, 32, 32, 4, 1, 40]
+
+marks[1] = 3048203840
+
+print(marks)
