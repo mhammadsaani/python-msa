439b98010583b8f56e3b59ef720fe606249b72d0
Hammad <m.hammadsaani@gmail.com>
2024-09-16 00:38:47 +0500

    week#11

---
+++ week 11/1.py
@@ -0,0 +1,56 @@
+
+# temp = "moosa"
+
+"""Tuple"""
+
+
+# unchangeable
+
+
+# temp = (1, 2, 3)
+
+# tempList = list(temp)
+
+# tempList.append(10)
+
+# temp = tuple(tempList)
+
+# print(temp)
+
+
+
+# history_marks, english_marks, python_marks, science_marks = (45, 50, 40, 45)
+
+# # history_marks = marks[0]
+# # english_marks = marks[1]
+# # python_marks = marks[2]
+# # science_marks = marks[3]
+
+# print(f'History Marks = {history_marks}, English Marks = {english_marks}, Python Marks = {python_marks} and Science Marks = {science_marks}')
+
+
+# (a, b) = (10, 20, 30)
+
+# a = 10
+# b = 20
+# c = 30
+# d = 40
+# e = 50
+
+
+# a, b, c, d, e= (10, 20, 30, 40, 50)
+
+
+tempList = [1, 2, 3, 4, 5, 6, 7, 8,9, 10]
+
+tempTuple = tuple(tempList)
+
+(a, b, c, *anything) = tempTuple
+
+
+print(anything)
+
+
+
+
+
---
+++ week 11/2.py
@@ -0,0 +1,24 @@
+### Set ==> unordered, unindexed and unchangeable*
+
+# myTuple = (1, 2, 3)
+
+mySet = {1}
+
+
+mySet.add(100)
+mySet.add(12)
+mySet.add(14)
+
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+print(mySet)
+
